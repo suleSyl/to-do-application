e1f6506be40feff1958610745b4b1f579f85e9ec
Şule Söylemezoğlu <sule.syl11@gmail.com>
2023-11-16 01:03:06 +0300

    CORS error solved

--- to-do-application-backend/src/main/java/com/todoapp/taskmanager/controller/TaskController.java
+++ to-do-application-backend/src/main/java/com/todoapp/taskmanager/controller/TaskController.java
@@ -4,17 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.todoapp.taskmanager.service.util.Constants;
 import com.todoapp.taskmanager.models.Task;
 import com.todoapp.taskmanager.service.TaskService;
@@ -27,11 +19,11 @@ import javax.annotation.PostConstruct;
 @RestController
 @Api(value = "Task Api documentation")
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TaskController {
 
     private final Logger log = LoggerFactory.getLogger(TaskController.class);
     private final TaskService taskService;
-
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
     }
